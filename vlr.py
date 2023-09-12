e26371d27f49448c6ade649b48f7882db79596fa
BrianJ-4 <brian.joshua03@gmail.com>
2023-09-11 19:22:05 -0700

    Added updateMatchDatabase() and searchDatabase

--- vlr.py
+++ vlr.py
@@ -1,5 +1,12 @@
 import requests
+import json
 from bs4 import BeautifulSoup
+import collections
+from collections import OrderedDict
+from datetime import datetime
+from dateutil.parser import parse
+import time
+
 
 def getVCTPlayers(minRounds = 200, agent = "all", mapid = "all", timespan = 60):
     minRounds = str(minRounds)
@@ -8,7 +15,6 @@ def getVCTPlayers(minRounds = 200, agent = "all", mapid = "all", timespan = 60):
     url = "https://www.vlr.gg/stats/?event_group_id=all&event_id=1189&series_id=all&region=all&country=all&min_rounds=" + minRounds + "&min_rating=1550&agent=" + agent + "&map_id=" + mapid + "&timespan=" + timespan + "d"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    #print(url)
 
     playerList = []
     players = soup.find('table', class_ = "wf-table mod-stats mod-scroll").find('tbody').find_all('tr')
@@ -200,13 +206,121 @@ def loadPlayerData(players): #to help with getPlayerDataMatch
         playerStats[playerDic["name"]] = playerDic
     return playerStats
 
+def updateMatchDatabase():
+    matches = OrderedDict()
 
+    url = "https://www.vlr.gg/matches/results"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-                    
+    pages = soup.find('div', class_ = "action-container-pages").findAll('a')
+    numTotalPages = int(pages[3].getText().strip())
 
+    format_string = "%a, %B %d, %Y" #For Datetime conversion
 
+    with open('matchesDatabase.json', 'r') as openfile:
+        matches = json.load(openfile)
+        matches = collections.OrderedDict(matches)
+    
+    secondToLatestDay = list(matches.keys())[1]
 
-                    
+    matches[list(matches.keys())[0]] = {} #Clear latest day in matches
 
+    beforesecondToLatestDay = True
 
+    for i in range(1, numTotalPages + 1): #Loop through every page
+        url = "https://www.vlr.gg/matches/results/?page=" + str(i)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        page = soup.find('div', class_ = "col mod-1")
+        datesInPage = page.findAll('div', class_ = "wf-label mod-large")
+        matchesInPage = page.findAll('div', class_ = "wf-card")
+        
+        for i in range(1, len(datesInPage) + 1): #Loop through all dates
+            date = datesInPage[i - 1].getText().strip()
+            
+            #Strips extra text from dates
+            if("Today" in date):                    
+                date = date[:date.index("\n") - 1]
+            elif("Yesterday" in date):  
+                date = date[:date.index("\n") - 1]
 
+            dateObj = datetime.strptime(date, format_string)
+            dateObj = dateObj.strftime("%Y-%m-%d")
+
+            #Loop until hit secondToLatestDay
+            if(dateObj == secondToLatestDay):
+                beforesecondToLatestDay = False
+                break     
+
+            matchesInDate = matchesInPage[i].findAll('a')
+            
+            matchOfDay = 0
+            dayMatches = {}
+
+            for match in matchesInDate: #Loop through matches in the date
+                matchDetails = {}
+
+                #Get all details to store
+                matchUrl = match['href']
+                matchId = matchUrl[1:matchUrl.find("/", matchUrl.find("/") + 1)]
+
+                teamA = match.findAll('div', class_ = "text-of")[0].getText().strip().upper()
+                teamB = match.findAll('div', class_ = "text-of")[1].getText().strip().upper()
+
+                eventText = match.find('div', class_ = "match-item-event text-of").getText().strip()
+                mainEvent = eventText[eventText.index("\t"):].strip().upper()
+                subEvent = eventText[:eventText.index("\t")].strip().upper()
+                
+                matchDetails['ID'] = matchId
+                matchDetails[teamA] = teamA
+                matchDetails[teamB] = teamB
+                matchDetails['Team1'] = teamA
+                matchDetails['Team2'] = teamB
+                matchDetails['Event'] = mainEvent
+                matchDetails['matchInEvent'] = subEvent
+                matchDetails['Date'] = dateObj
+
+                #If date already exists in matches then do not override it
+                #This is to deal with matches from the same day that stretch over two pages
+                if(dateObj in matches):                              
+                    numMatches = (len(matches[dateObj]) - 1)
+                    matches[dateObj][numMatches + 1] = matchDetails
+                
+                else:
+                    dayMatches[matchOfDay] = matchDetails
+                    matchOfDay += 1
+                    matches[dateObj] = dayMatches
+
+            matches.move_to_end(dateObj, last = True)
+
+        #Sort matches by date and write to file
+        matches = OrderedDict(sorted(matches.items(), key = lambda x: parse(x[0]), reverse = True))
+        with open("matchesDatabase.json", "w") as outfile:
+            json.dump(matches, outfile)
+
+        if(beforesecondToLatestDay == False):
+            break
+
+def searchDatabase(**kwargs):
+    with open('matchesDatabase.json', 'r') as openfile:
+        matches = json.load(openfile)
+        matches = collections.OrderedDict(matches)
+
+    validFilters = OrderedDict()
+    for arg in kwargs:
+        if kwargs[arg] != "":
+            validFilters[arg] = kwargs[arg]           
+
+    results = []
+    for date in matches:
+        for match in matches[date]:
+            valid = True
+            for key, value in validFilters.items():
+                if matches[date][match][key] != value:
+                    valid = False
+                    break
+            if valid:
+                
+                results.append(matches[date][match])
+    return results
