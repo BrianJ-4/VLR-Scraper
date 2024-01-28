249d767ad7fe823d0f20c866b9cbe204186b24cb
BrianJ-4 <brian.joshua03@gmail.com>
2024-01-27 21:51:35 -0800

    Removed unecessary methods

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from vlr import *
 import time
 
-# players = getVCTPlayers(agent = "astra", minRounds = 0, timespan = "all")
+# players = getVCTPlayers(agent = "astra", minRounds = 100, timespan = "all")
 
 # for name in players:
 #     print(name)
@@ -12,31 +12,42 @@ print()
 
 start = time.time()
 
+
 #print(getLiveMatches())
-#getLiveMatchesVLR()
-#print(getMatchStats(248275))
+# stats = getMatchStats(7095)
+# print(stats)
 #print(getPlayerStats(11225))
 #print(getLiveMatches())
 
-updateMatchDatabase() 
+#updateMatchDatabase() 
 
-# team01 = ""
-# team02 = ""
-# mainEvent = ""
-# date = "2020-12-06"
+# team01 = "100 tHievEs"
+# team02 = None
+# mainEvent = None
+# date = None
 
-# filtered_entries = searchDatabase(Team1 = team01, Team2 = team02, Event = mainEvent, Date = date)
-# filtered_entries += (searchDatabase(Team1 = team02, Team2 = team01, Event = mainEvent, Date = date))
+# print(team01)
+# print(team02)
+# print(mainEvent)
+# print(date)
+# filtered_entries = searchMatchDatabase(Team1 = team01, Team2 = team02, Event = mainEvent, Date = date)
+# filtered_entries += (searchMatchDatabase(Team1 = team02, Team2 = team01, Event = mainEvent, Date = date))
 
 # for entry in filtered_entries:
 #     print(entry)
 #     print()
 
+#updatePlayerDatabase(1)
 
+# print(searchPlayerDatabase(0, "potter"))
 
+# getArticle("https://www.vlr.gg/264266/riot-previews-major-balance-changes-to-11-agents")
+
+search("vct", "all")
 
 end = time.time()
 print("Time elapsed :",
+
       (end-start) * 10**3, "ms")
 
 print()
